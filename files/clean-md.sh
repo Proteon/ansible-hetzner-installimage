e193cc0c3379cb04d08292cfebf49ab1097ed4f4
Alwyn Kik <alwyn@firelay.com>
2019-04-02 18:19:09 +0200

    Various fixes/improvements

--- files/clean-md.sh
+++ files/clean-md.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop and remove arrays
-my_mds=$(find /dev -type b -name 'md*')
+my_mds=( $(find /dev -type b -name 'md*') )
 
 # Cleanly exit if there are no active arrays
 [ -z "$my_mds" ] && exit
